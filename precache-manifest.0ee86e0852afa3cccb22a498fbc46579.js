6fce43a3613cbf6fa271215f087ca9083f985df4
Keith Ratner <keith.ratner@gmail.com>
2019-12-23 19:00:26 -0800

    Updates

---
+++ precache-manifest.0ee86e0852afa3cccb22a498fbc46579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea15824d0217778602ee0bafd09c85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6622db9f1017fe8abad8",
+    "url": "/static/css/main.bf95859f.chunk.css"
+  },
+  {
+    "revision": "ceaa6f863eeb4706087d",
+    "url": "/static/js/2.8ccf9b42.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.8ccf9b42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6622db9f1017fe8abad8",
+    "url": "/static/js/main.eed190fd.chunk.js"
+  },
+  {
+    "revision": "c47278d37ba02f1a8a3d",
+    "url": "/static/js/runtime-main.7683ef5f.js"
+  }
+]);
