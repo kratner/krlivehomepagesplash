ac7b6e7c526dbd79a3db1325f83b79b063e32103
Keith Ratner <keith.ratner@gmail.com>
2019-12-24 14:47:03 -0800

    Fixed unit test

--- src/__tests__/SplashLinksMenu.test.js
+++ src/__tests__/SplashLinksMenu.test.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import SplashLinksMenu from "../components/SplashLinksMenu";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducer from "../reducers/index";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 describe("SplashLinksMenu", () => {
   it("renders correctly", () => {
-    const wrapper = shallow(<SplashLinksMenu />);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <SplashLinksMenu />
+      </Provider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
